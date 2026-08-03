9cc66b5de75801b117a5a9564517b80b5f5690c5
Omarz <150090543+O-Alaa@users.noreply.github.com>
2026-08-04 00:13:56 +0300

    Update config.py

--- client/config.py
+++ client/config.py
@@ -1,2 +1,10 @@
-#API_URL="http://127.0.0.1:8000"
-API_URL="https://medical-chatbot-ndeq.onrender.com"
+import os
+
+
+API_URL = os.getenv(
+    "MEDICAL_API_URL",
+    "https://medical-chatbot-ndeq.onrender.com",
+).rstrip("/")
+
+UPLOAD_TIMEOUT_SECONDS = 600
+ASK_TIMEOUT_SECONDS = 180
